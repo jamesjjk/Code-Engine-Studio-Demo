ffd6973c63dcd191d60cf147c97a52df13f2c3be
jamesjjk <jamesjjk@jamess-MacBook-Pro.local>
2016-01-30 11:11:07 +0700

    Fix to the demo

--- actions/apiActions.js
+++ actions/apiActions.js
@@ -9,5 +9,6 @@ export default function getFriends(actionContext, payload, done) {
 
 export function sayHello(actionContext, payload, done) {
     actionContext.dispatch('HELLO_FRIEND', payload);
+    //ALWAY RUN THE CALLBACK (any errors can be wrapped in an error object)
     done()
 }
